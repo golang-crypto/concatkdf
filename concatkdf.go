e82c75cfaeb69fd627450ad6e761c2f0776388da
Igor Simon <iasimon.sp@gmail.com>
2025-07-04 14:09:37 -0300

    add documentation for the Key function

--- concatkdf.go
+++ concatkdf.go
@@ -11,6 +11,15 @@ import (
 	"hash"
 )
 
+// Key derives a key from the given hash, secret and context info, returning a []byte
+// of length keyLength that can be used as cryptographic key.
+//
+// A salt may be provided through a function pre-initializing the hash block:
+//
+//	key, err := concatkdf.Key(func() hash.Hash { return hmac.New(sha256.New, salt) }, secretMaterial, info, keyLen)
+//	if err != nil {
+//		// ...
+//	}
 func Key[Hash hash.Hash](h func() Hash, secret []byte, info string, keyLength int) ([]byte, error) {
 	out := make([]byte, 0, keyLength)
 	hasher := h()
